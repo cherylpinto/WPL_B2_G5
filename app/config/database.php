8008296a89dc92242ace6a39da78c5c5925d48c4
cherylpinto <cp16november@gmail.com>
2025-04-14 19:51:07 +0530

    Google Auth

--- app/config/database.php
+++ app/config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'cherylpinto');
 define('DB_DATABASE', 'restaurant_db');
 
 class Database {
